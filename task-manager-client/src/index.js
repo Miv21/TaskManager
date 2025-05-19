37242d568b239bcfd03a7a323b23661695580605
Miv <mosheminska@gmail.com>
2025-05-19 18:59:45 +0300

    Исправление проверки прав доступа вошедшего пользователя

--- task-manager-client/src/index.js
+++ task-manager-client/src/index.js
@@ -3,21 +3,21 @@ import { createRoot } from 'react-dom/client';
 import { ChakraProvider } from '@chakra-ui/react';
 import { BrowserRouter } from 'react-router-dom';
 import App from './App';
-//import './index.css'; 
-
+import { AuthProvider } from './hooks/useAuth'; // <-- правильный импорт AuthProvider
 
 const container = document.getElementById('root');
 if (!container) {
   throw new Error('Не найден <div id="root"> в public/index.html');
 }
 
-
 const root = createRoot(container);
 root.render(
   <React.StrictMode>
     <ChakraProvider>
       <BrowserRouter>
-        <App />
+        <AuthProvider>  
+          <App />
+        </AuthProvider>
       </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>
