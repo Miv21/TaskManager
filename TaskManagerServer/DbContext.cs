e16dcc8d0e349f1d69e3f4d480afb13b571b65d1
Miv <mosheminska@gmail.com>
2025-05-28 23:16:09 +0300

    Создание задачь

--- TaskManagerServer/DbContext.cs
+++ TaskManagerServer/DbContext.cs
@@ -11,6 +11,47 @@ namespace TaskManagerServer
         public DbSet<TaskCard> Tasks { get; set; }
         public DbSet<TaskResponse> TaskResponses { get; set; }
         public DbSet<Position> Positions { get; set; }
+
         public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Role>().HasData(
+                new Role { Id = 1, Name = "Employer" },
+                new Role { Id = 2, Name = "Employee" },
+                new Role { Id = 3, Name = "Admin" },
+                new Role { Id = 4, Name = "TopeEmployer" }
+            );
+
+            // Связь TaskCard -> Employer
+            modelBuilder.Entity<TaskCard>()
+                .HasOne(t => t.Employer)
+                .WithMany(u => u.CreatedTasks)
+                .HasForeignKey(t => t.EmployerId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Связь TaskCard -> TargetUser 
+            modelBuilder.Entity<TaskCard>()
+                .HasOne(t => t.TargetUser)
+                .WithMany()
+                .HasForeignKey(t => t.TargetUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            // Связь TaskResponse -> TaskCard
+            modelBuilder.Entity<TaskResponse>()
+                .HasOne(r => r.Task)
+                .WithMany(t => t.Responses)
+                .HasForeignKey(r => r.TaskId)
+                .OnDelete(DeleteBehavior.Cascade);
+
+            // TaskResponse → Employee
+            modelBuilder.Entity<TaskResponse>()
+                .HasOne(r => r.Employee)
+                .WithMany(u => u.Responses)
+                .HasForeignKey(r => r.EmployeeId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
     }
 }
---
+++ TaskManagerServer/Migrations/20250528114514_dbcontext.cs
@@ -0,0 +1,97 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagerServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class dbcontext : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskResponses_Users_EmployeeId",
+                table: "TaskResponses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_EmployerId",
+                table: "Tasks");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TargetUserId",
+                table: "Tasks",
+                type: "integer",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Tasks_TargetUserId",
+                table: "Tasks",
+                column: "TargetUserId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskResponses_Users_EmployeeId",
+                table: "TaskResponses",
+                column: "EmployeeId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_EmployerId",
+                table: "Tasks",
+                column: "EmployerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_TargetUserId",
+                table: "Tasks",
+                column: "TargetUserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Restrict);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_TaskResponses_Users_EmployeeId",
+                table: "TaskResponses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_EmployerId",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_TargetUserId",
+                table: "Tasks");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Tasks_TargetUserId",
+                table: "Tasks");
+
+            migrationBuilder.DropColumn(
+                name: "TargetUserId",
+                table: "Tasks");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_TaskResponses_Users_EmployeeId",
+                table: "TaskResponses",
+                column: "EmployeeId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_EmployerId",
+                table: "Tasks",
+                column: "EmployerId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
