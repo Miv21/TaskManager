342ca0c57ca6c1f4ad1e2172a0ab540dc99d1cb0
Miv <mosheminska@gmail.com>
2025-06-15 18:03:48 +0300

    Изменения метода отправки карточек и возможность скачивать файлы из TaskResponse

---
+++ TaskManagerServer/Migrations/20250612151746_TaskResp.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskManagerServer.Migrations
+{
+    /// <inheritdoc />
+    public partial class TaskResp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "EmployerId",
+                table: "TaskResponses",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "EmployerId",
+                table: "TaskResponses");
+        }
+    }
+}
---
+++ TaskManagerServer/Models/TaskCardDto.cs
@@ -0,0 +1,17 @@
+﻿namespace TaskManagerServer.Models
+{
+    public class TaskCardDto
+    {
+        public int? TaskId { get; set; }
+        public int? ResponseId { get; set; }
+        public string Title { get; set; } = default!;
+        public string Description { get; set; } = default!;
+        public string? ResponseText { get; set; }
+        public DateTime Deadline { get; set; }
+        public DateTime TaskCreationTime { get; set; }
+        public string? FileUrl { get; set; }
+        public string? OriginalFileUrl { get; set; }
+        public int? TargetUserId { get; set; }
+        public string Type { get; set; } = default!;
+    }
+}
